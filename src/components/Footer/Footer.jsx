12234614555f9422b889abcbea8e7e991aee5fc1
Revaz Gelashvili Laptop <revazgelashviliofficial@gmail.com>
2025-09-02 03:23:40 +0400

    delete logo in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-import Logo from "../../assets/coin-logo.svg";
 import GitHub from "../../assets/github.svg";
 import Telegram from "../../assets/telegram.svg";
 import "./Footer.scss";
@@ -7,11 +6,8 @@ export default function Footer() {
   return (
     <div className="footer h-16 flex items-center text-center bg-gray-100 justify-center fixed bottom-0 left-0 w-full">
       <div className="flex items-center text-center justify-between w-sm md:w-150 lg:w-200 xl:w-300">
-        <div className="content flex justify-center items-center gap-2">
+        <div className="content ">
           <h3>RateNest</h3>
-          <a href="#" className="hidden md:block">
-            <img src={Logo} alt="logo" className="w-10 h-10 " />
-          </a>
         </div>
         <div className="about">
           <p className="flex gap-1">
