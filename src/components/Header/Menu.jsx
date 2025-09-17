da99befb3d0c9b7b41446151414248b9286abac5
Revaz <revazgelashviliofficial@gmail.com>
2025-09-18 01:24:54 +0400

    sidebar fix

--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,11 +1,12 @@
 import Close from "../../assets/close.svg";
 import Sun2 from "../../assets/sun.svg";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 export default function Menu({ isOpen, toggleSideBar }) {
   return (
     <div
-      className={`sideBar flex flex-col justify-between z-30 fixed top-0 left-0 h-full w-64 bg-white text-black transform 
+      className={`sideBar flex flex-col justify-between z-50 fixed top-0 left-0 h-[100vh] w-64 bg-white text-black transform 
       ${isOpen ? "translate-x-0" : "-translate-x-full"} 
       transition-transform duration-300`}
     >
@@ -18,18 +19,18 @@ export default function Menu({ isOpen, toggleSideBar }) {
         />
       </div>
       <ul className="p-4 flex flex-col gap-5">
+        <Link to="/">
+          <li className="work">Home</li>
+        </Link>
         <a href="#">
-          <li className="py-2">Exchange</li>
+          <li className="py-2">News</li>
         </a>
         <a href="#">
           <li className="py-2">Rates</li>
         </a>
-        <a href="#">
-          <li className="py-2">Charts</li>
-        </a>
-        <a href="#">
-          <li className="py-2">News</li>
-        </a>
+        <Link to="/register">
+          <li className="work">Register</li>
+        </Link>
       </ul>
       <img src={Sun2} alt="Sun" className="w-12 h-12 cursor-pointer" />
     </div>
